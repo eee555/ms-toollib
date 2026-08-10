994bd2305235dbe08bca4d34dbcc0187bd720ef0
👑➕🍋 <50390200+eee555@users.noreply.github.com>
2026-08-10 21:16:28 +0800

    release: 发布新版本

--- base/src/videos/base_video.rs
+++ base/src/videos/base_video.rs
@@ -1236,6 +1236,8 @@ pub fn valid_time_period(software: &str) -> Result<(String, String), String> {
         "元3.3.2" => Ok(("1783353600".to_string(), "4102415999".to_string())),
         // 2026-07-10 00:00:00 发布
         "元3.3.3" => Ok(("1783612800".to_string(), "4102415999".to_string())),
+        // 2026-08-10 00:00:00 发布
+        "元3.3.4" => Ok(("1786291200".to_string(), "4102415999".to_string())),
         _ => Err(String::from("Unknown software: ") + software),
     }
 }
@@ -1272,6 +1274,7 @@ impl<T> BaseVideo<T> {
             || self.software == "元3.3.1"
             || self.software == "元3.3.2"
             || self.software == "元3.3.3"
+            || self.software == "元3.3.4"
         {
             if self.checksum.iter().all(|&e| e == self.checksum[0]) {
                 // 大概率是使用了测试用的校验和
